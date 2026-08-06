31fe2c72b366190af28273b33e5981c92f2d18bd
devchagas <dev.gchagas@gmail.com>
2026-08-06 18:17:52 -0300

    Deploy

--- src/main/java/com/web/workshop/config/TestConfig.java
+++ src/main/java/com/web/workshop/config/TestConfig.java
@@ -12,7 +12,7 @@ import java.time.Instant;
 import java.util.Arrays;
 
 @Configuration
-@Profile("test")
+@Profile({"dev", "test"})
 public class TestConfig implements CommandLineRunner {
 
     @Autowired
